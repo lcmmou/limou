59f239c7240bdd462de3626893fb3624317c0fcc
licenmou <1984978980@qq.com>
2023-11-02 14:50:02 +0800

    ten commit

---
+++ src/main/java/com/example/limou/LimouApplication.java
@@ -0,0 +1,13 @@
+package com.example.limou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/limou/LimouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.limou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
